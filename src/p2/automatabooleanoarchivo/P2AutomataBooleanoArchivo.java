cacf729c98825c76109b1ed80fa06e8e0ab63055
Mauricio Garduño <mau.gar.gonz@icloud.com>
2019-03-06 03:18:48 -0600

    ya se armo

---
+++ src/p2/automatabooleanoarchivo/P2AutomataBooleanoArchivo.java
@@ -0,0 +1,20 @@
+package p2.automatabooleanoarchivo;
+
+import GUI.Main;
+
+/**
+ *
+ * @author Jedidian
+ */
+public class P2AutomataBooleanoArchivo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main inicio = new Main();
+        inicio.setVisible(true);
+    }
+    
+}
